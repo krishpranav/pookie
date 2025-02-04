885cf3c585ce6137755272821532862a96ecc97f
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-04 15:57:16 +0530

    packages: pookie[root]

--- packages/pookie/index.js
+++ packages/pookie/index.js
@@ -1,11 +1,12 @@
 var Module = require('module').Module;
 
-var fallback = Module._resolveFileName;
+var fallback = Module._resolveFilename;
 
 module.exports = require('./dist/pookie.node.js');
 
-Module._resolveFileName = function(name, from) {
-    if (name == "pookie") {
-        return __filename;
-    }
-}
+Module._resolveFilename = function (name, from) {
+  if (name == "pookie") {
+    return __filename;
+  };
+  return fallback.apply(Module, arguments);
+};
